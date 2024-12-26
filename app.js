dd6a473e277c4612ab6d25a2f349cb94f48de162
Orhan Yardim <stj.oyardim@havelsan.com.tr>
2024-12-26 13:56:06 +0300

    modified files

--- app.js
+++ app.js
@@ -8,17 +8,31 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const blogTopics = ['technology', 'travel', 'health', 'education', 'recipes'];
+
 // Ana sayfa rotası
 app.get('/', (req, res) => {
-  const posts = JSON.parse(fs.readFileSync('data/posts.json', 'utf-8'));
+  let posts = [];
+  blogTopics.forEach(topic => {
+    let topicPosts = JSON.parse(fs.readFileSync(`data/${topic}.json`, 'utf-8'));
+    topicPosts = topicPosts.map(post => {
+      return { ...post, topic }; // Her posta topic bilgisi ekle
+    });
+    posts = posts.concat(topicPosts);
+  });
   res.render('index', { posts: posts });
 });
 
 // Yazı detay rotası
-app.get('/post/:id', (req, res) => {
-  const posts = JSON.parse(fs.readFileSync('data/posts.json', 'utf-8'));
-  const post = posts.find(p => p.id == req.params.id);
-  res.render('post', { post: post });
+app.get('/post/:topic/:id', (req, res) => {
+  const { topic, id } = req.params;
+  const posts = JSON.parse(fs.readFileSync(`data/${topic}.json`, 'utf-8'));
+  const post = posts.find(p => p.id == id);
+  if (post) {
+    res.render('post', { post: post });
+  } else {
+    res.status(404).send('Post not found');
+  }
 });
 
 app.listen(port, () => {
